0625fea8d9115de46984b57dea4c21c55b710d84
libbyberrie <libbyberrie@me.com>
2023-04-16 23:44:12 +1000

    Add in all the form fields, unstyled. The form data returns a json object which is printed underneath the form for reference.

--- src/components/fields/CheckBox.js
+++ src/components/fields/CheckBox.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function Radio(props) {
+export default function Checkbox(props) {
   const { fieldName, instruction, options, required, register } = { ...props };
 
   const optionButtons = options.map((entry, index) => {
     return (
       <div key={index}>
         <input
-          type="radio"
+          type="checkbox"
           id={`${fieldName}-${index}`}
-          className="form form-radio"
+          className="form form-checkbox"
           name={fieldName}
           value={entry}
           {...register(fieldName, { required: required })}

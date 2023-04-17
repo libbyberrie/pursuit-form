9f730e5abd9314ee07fd856e6db4b10b04538565
libbyberrie <libbyberrie@me.com>
2023-04-17 20:55:19 +1000

    please appease netlify gods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {/* Netlify needs a html output of the form as well as the react form, 
+    in order to get the forms to show up, as React is rendering this client-side.
+    This hidden markup is used to ensure the JSX version of the form is detected in Netlify */}
+    <form
+      name="Registration request"
+      netlify
+      data-netlify="true"
+      enc-type="multipart/form-data"
+      netlify-honeypot="bot-field"
+      hidden
+    >
+      <input type="radio" name="work-plan-approval" />
+      <input type="checkbox" name="reasons" />
+      <textarea name="aims" />
+      <input type="radio" name="adjustments-required" />
+      <textarea name="accessibility-details" />
+      <input type="file" name="documentation" />
+    </form>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
